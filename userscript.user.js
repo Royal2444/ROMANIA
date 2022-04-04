dd7dadac012c3bdd820f98e4f0be76d23859b909
Royal2444 <102917263+Royal2444@users.noreply.github.com>
2022-04-04 15:25:49 +0300

    Update userscript.user.js

--- userscript.user.js
+++ userscript.user.js
@@ -14,7 +14,7 @@ if (window.top !== window.self) {
         (function () {
             const i = document.createElement("img");
             i.src = "https://cdn.discordapp.com/attachments/959873499776831578/960515289064276038/romania2dacie.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
             console.log(i);
             return i;
         })())
